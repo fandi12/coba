e8f9654b84da87808e379f6522893f4aa3fe789c
fandi12 <37774326+fandi12@users.noreply.github.com>
2018-05-03 19:00:31 +0700

    first commit

---
+++ coba.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>COBA 2</title>
+	<link rel="stylesheet" type="text/css" href="style2.css">
+</head>
+<body>
+	<header>
+		
+	</header>
+
+	<section>
+		<div class="makan"></div>
+		<div class="makan"></div>
+		<div class="makan"></div>
+	</section>
+
+	<aside>
+			
+	</aside>
+
+	<footer>
+		
+	</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ini Judul</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		
+	</header>
+
+	<nav>
+		<ul>
+			<li><a href="">HOME</a></li>
+			<li><a href="">HOME</a></li>
+			<li><a href="">HOME</a></li>
+		</ul>
+	</nav>
+
+	<aside>
+		
+	</aside>
+
+	<section>
+		<p> adasbjabbksakwnaigfbakayfvajkayvakwfgawukfawfwajgfuwfaifhaihfiwfqwifgwf </p>
+	</section>
+
+	<footer>
+		
+	</footer>
+</body>
+</html>
